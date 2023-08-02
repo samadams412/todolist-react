f4f1a590d9ccffda8618f7e2c73f0f3917a87e65
Samuel Adams <samueladams412@gmail.com>
2023-08-01 19:23:17 -0500

    Added todo code to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from "react";
+import "./assets/style.css"
 
-  return (
+export default function App(){
+  //state variable is immutable, it cannot be updated
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id) {
+          return {...todo, completed};
+        }
+        return todo;
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    //when you need the current value of state you must pass a function not just a value
+    setTodos((currentTodos => {
+      return [
+        ...todos, {id: crypto.randomUUID(), title: newItem, completed: false}
+      ]
+    }))
+    setNewItem("");
+   
+  }
+  console.log(todos);
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">Add Item</label>
+          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"></input> 
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1>ToDo List</h1>
+      <ul className="list">
+        {/* Short circuiting to determine if todos contains any todo */}
+        {todos.length === 0 && "No Todos"}
+        {todos.map(todo => {
+          return(
+            //react uses key so that we know which todo element to update
+            //not a good idea to use index of the array because if we delete there could be bugs
+            <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.completed} 
+              onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+                {todo.title}
+            </label>
+            {/* Must pass the function to onClick because otherwise it will call deleteTodo right away */}
+              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
+          </li>
+          )
+        })}
+      </ul>
     </>
-  )
-}
-
-export default App
+    );
+};
