f5b1cbf49c6ab786a79345718314d37806d8e311
Samuel Adams <samueladams412@gmail.com>
2023-08-01 19:53:58 -0500

    Modularized the code into seperate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,52 @@
-
-import { useState } from "react";
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./components/ToDoForm"
 import "./assets/style.css"
+import { TodoList } from "./components/ToDoList"
 
-export default function App(){
-  //state variable is immutable, it cannot be updated
-  const [newItem, setNewItem] = useState("");
-  const [todos, setTodos] = useState([]);
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
 
-  function toggleTodo(id, completed){
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+
+  function toggleTodo(id, completed) {
     setTodos(currentTodos => {
       return currentTodos.map(todo => {
-        if(todo.id === id) {
-          return {...todo, completed};
+        if (todo.id === id) {
+          return { ...todo, completed }
         }
-        return todo;
+
+        return todo
       })
     })
   }
 
-  function deleteTodo(id){
+  function deleteTodo(id) {
     setTodos(currentTodos => {
       return currentTodos.filter(todo => todo.id !== id)
     })
   }
 
-  function handleSubmit(e){
-    e.preventDefault();
-    //when you need the current value of state you must pass a function not just a value
-    setTodos((currentTodos => {
-      return [
-        ...todos, {id: crypto.randomUUID(), title: newItem, completed: false}
-      ]
-    }))
-    setNewItem("");
-   
-  }
-  console.log(todos);
-  return(
+  return (
     <>
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">Add Item</label>
-          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"></input> 
-        </div>
-        <button className="btn">Add</button>
-      </form>
-      <h1>ToDo List</h1>
-      <ul className="list">
-        {/* Short circuiting to determine if todos contains any todo */}
-        {todos.length === 0 && "No Todos"}
-        {todos.map(todo => {
-          return(
-            //react uses key so that we know which todo element to update
-            //not a good idea to use index of the array because if we delete there could be bugs
-            <li key={todo.id}>
-            <label>
-              <input type="checkbox" checked={todo.completed} 
-              onChange={e => toggleTodo(todo.id, e.target.checked)}/>
-                {todo.title}
-            </label>
-            {/* Must pass the function to onClick because otherwise it will call deleteTodo right away */}
-              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
-          </li>
-          )
-        })}
-      </ul>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
-    );
-};
+  )
+}
